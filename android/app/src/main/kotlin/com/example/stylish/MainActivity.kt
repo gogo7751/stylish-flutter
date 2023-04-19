8c5b02dd80934c67576516b52c074e3cb10045e6
gogo7751 <gogo7751@hotmail.com>
2023-04-19 14:17:48 +0800

    add andorid platform channel

--- android/app/src/main/kotlin/com/example/stylish/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/stylish/MainActivity.kt
@@ -1,6 +1,24 @@
 package com.example.stylish
-
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.stylish"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler{
+      // This method is invoked on the main thread.
+      call, result ->
+      if (call.method == "helloFromNative") {
+        val name = call.argument<String>("name")
+        val greeting = "Hello, $name from Native!"
+        result.success(greeting)
+      } else {
+        result.notImplemented()
+      }
+    }
+  }
 }
--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:stylish/widget/home_page_banner.dart';
 import 'package:stylish/widget/home_page_prodcut_list.dart';
 import 'package:stylish/widget/app_bar.dart';
@@ -23,14 +24,55 @@ class MyHomePage extends StatelessWidget {
             create: (_) => ProductBloc(productRepo)..add(ProductLoadEvent()),
           ),
         ],
-        child: (const Scaffold(
+        child: (Scaffold(
           appBar: MyAppBar(),
           body: Column(
             children: [
               BannerList(),
+              PlatformChannel(),
               HomePageProductList(),
             ],
           ),
         )));
+  }
+}
+
+class PlatformChannel extends StatefulWidget {
+  const PlatformChannel({Key? key}) : super(key: key);
+
+  @override
+  PlatformChannelState createState() => PlatformChannelState();
+}
+
+class PlatformChannelState extends State<PlatformChannel> {
+  static const platform = MethodChannel('com.example.stylish');
+  String _message = 'No message received.';
+
+  Future<void> _helloFromNative(String name) async {
+    String message;
+    try {
+      final String result =
+          await platform.invokeMethod('helloFromNative', {'name': name});
+      message = 'Native message: $result';
+    } on PlatformException catch (e) {
+      message = 'Error: ${e.message}';
+    }
+
+    setState(() {
+      _message = message;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ElevatedButton(
+          child: const Text('Say hello from Native!'),
+          onPressed: () => _helloFromNative('Flutter'),
+        ),
+        Text(_message),
+      ],
+    );
   }
 }
